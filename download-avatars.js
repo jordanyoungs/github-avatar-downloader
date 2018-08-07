32435f40cad6b30925477c52f3152350536235f0
Jordan Youngs <jordankyoungs@gmail.com>
2018-08-07 00:03:14 +0000

    Halfway through step 5, returns the body as an array of objects

--- download-avatars.js
+++ download-avatars.js
@@ -1,3 +1,32 @@
 var request = require("request");
+var fs = require("fs");
+var authentication = '-u jordanyoungs:da296213625280444dcfaa6cdae0a7e6b21d9d84 ';
 
 console.log('Welcome to the GithubAvatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+  // var error = null;
+  // request.get("https://api.github.com/repos/" + repoOwner + "/" + repoName
+  //   + "/contributors/" + "?access_token=" + token)
+  // .on('error', function (err) {
+  //   error = err;
+  // })
+  // .on('response', function (response) {
+  //   cb(error, response);
+  //   })
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
